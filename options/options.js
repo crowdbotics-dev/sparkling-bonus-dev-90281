90aa16e33b4e4303fedd056df6c9e66c3b7e46bb
Crowdbotics <engineering@crowdbotics.com>
2023-06-10 23:08:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sparkling-bonus-dev-90281",
+  url: "https://sparkling-bonus-dev-90281.botics.co",
+  api: "https://sparkling-bonus-dev-90281.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
